892008d75cfa6db71f5400240f440de6ccaed63a
mohanmdu <mohanraj2014@gmail.com>
2019-05-12 21:54:09 +0530

    ui integration

--- springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/controller/UserController.java
+++ springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tem.springbootcrudrest.exception.ResourceNotFoundException;
@@ -29,6 +30,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@GetMapping(value="/")
+	public String home() {
+	return "index";
+	} 
 	
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
