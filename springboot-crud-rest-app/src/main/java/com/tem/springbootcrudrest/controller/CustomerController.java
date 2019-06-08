60bb57d5919949c6bcb97acffa57164ca290968a
mohanmdu <mohanraj2014@gmail.com>
2019-06-08 09:25:25 +0530

    customer updates

---
+++ springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/controller/CustomerController.java
@@ -0,0 +1,78 @@
+package com.tem.springbootcrudrest.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tem.springbootcrudrest.exception.ResourceNotFoundException;
+import com.tem.springbootcrudrest.model.Customer;
+import com.tem.springbootcrudrest.repository.CustomerRepository;
+
+
+@RestController
+@RequestMapping("/api/v2")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@GetMapping("/customers")
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+
+	@GetMapping("/customers/{customerId}")
+	public ResponseEntity<Customer> getUserById(
+			@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
+		Customer user = customerRepository.findById(userId)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found :: " + userId));
+		return ResponseEntity.ok().body(user);
+	}
+
+	@PostMapping("/customers")
+	public Customer createUser(@Valid @RequestBody Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	@PutMapping("/customers/{id}")
+	public ResponseEntity<Customer> updateUser(
+			@PathVariable(value = "id") Long userId,
+			@Valid @RequestBody Customer userDetails) throws ResourceNotFoundException {
+		Customer user = customerRepository.findById(userId)
+		        .orElseThrow(() -> new ResourceNotFoundException("User not found :: " + userId));
+		
+		//user.setEmailId(userDetails.getEmailId());
+		//user.setLastName(userDetails.getLastlName());
+		//user.setFirstName(userDetails.getFirstName());
+		//user.setUpdatedAt(new Date());
+		final Customer updatedUser = customerRepository.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+
+	@DeleteMapping("/customers/{id}")
+	public Map<String, Boolean> deleteUser(
+			@PathVariable(value = "id") Long userId) throws ResourceNotFoundException {
+		Customer user = customerRepository.findById(userId)
+		        .orElseThrow(() -> new ResourceNotFoundException("User not found :: " + userId));
+
+		customerRepository.delete(user);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
+}
---
+++ springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/model/Customer.java
@@ -0,0 +1,164 @@
+package com.tem.springbootcrudrest.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "customers")
+@EntityListeners(AuditingEntityListener.class)
+public class Customer {
+	private long customerId;
+	private String customerName;
+	private String PAN;
+	private String TAN;
+	private String KYC;
+	private String IEC;
+	private Date createdAt;
+	private String createdby;
+	
+	@Id
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Customer_Id", unique = true, nullable = false)
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	@Column(name = "Customer_name", nullable = false)
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	@Column(name = "Created_At", nullable = true)
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	@Column(name = "Created_by", nullable = true)
+	public String getCreatedby() {
+		return createdby;
+	}
+	public void setCreatedby(String createdby) {
+		this.createdby = createdby;
+	}
+	@Column(name = "pan", nullable = true)
+	public String getPAN() {
+		return PAN;
+	}
+	public void setPAN(String pAN) {
+		PAN = pAN;
+	}
+	@Column(name = "tan", nullable = true)
+	public String getTAN() {
+		return TAN;
+	}
+	public void setTAN(String tAN) {
+		TAN = tAN;
+	}
+	@Column(name = "kyc", nullable = true)
+	public String getKYC() {
+		return KYC;
+	}
+	public void setKYC(String kYC) {
+		KYC = kYC;
+	}
+	@Column(name = "iec", nullable = true)
+	public String getIEC() {
+		return IEC;
+	}
+	public void setIEC(String iEC) {
+		IEC = iEC;
+	}
+	
+	/*@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "first_name", nullable = false)
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	@Column(name = "last_name", nullable = false)
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	@Column(name = "email_address", nullable = false)
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	@Column(name = "created_at", nullable = false)
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	@Column(name = "created_by", nullable = false)
+	@CreatedBy
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+	@Column(name = "updated_at", nullable = false)
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@Column(name = "updated_by", nullable = false)
+	@LastModifiedBy
+	public String getUpdatedby() {
+		return updatedby;
+	}
+	public void setUpdatedby(String updatedby) {
+		this.updatedby = updatedby;
+	}*/
+}
