031f407c42abe1ed822157ef4b7d5e07905651d2
sivaraman.r <sivaraman.r@LP-5CD9126FG8.HCLT.CORP.HCL.IN>
2019-10-05 16:14:59 +0530

    changes

--- springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/controller/CustomerInvoiceCrudRestApi.java
+++ springboot-crud-rest-app/src/main/java/com/tem/springbootcrudrest/controller/CustomerInvoiceCrudRestApi.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,6 +50,7 @@ public class CustomerInvoiceCrudRestApi {
 		return customerInvoiceList;
 	}
 	
+	@RequestMapping(path = "/vendorinvoicelistincustomer", method = RequestMethod.GET)
 	public List<CustomerInvoice> getVendorInvoiceList(@RequestParam String fromdate, @RequestParam String todate,
 			@RequestParam String customername) {
 
